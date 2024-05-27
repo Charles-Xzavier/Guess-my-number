116f9cd1d4e3be14230e292960bf6aba3c294571
Charles-Xzavier <charlesryan929@gmail.com>
2024-05-27 21:24:14 +0100

    added levels

--- script.js
+++ script.js
@@ -59,3 +59,115 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '12rem';
 });
+
+//for medium level
+let secretNumber2 = Math.trunc(Math.random() * 50) + 1;
+let score2 = 10;
+let highscore2 = 0;
+
+document.querySelector('.mediumbox').addEventListener('click', function () {
+  document.querySelector('.between').textContent = '(From 1 to 50)';
+  document.querySelector('.score').textContent = 10;
+
+  document.querySelector('.check').addEventListener('click', function () {
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess, typeof guess);
+
+    //when no input
+    if (!guess) {
+      displayMessage('⛔ No number!');
+
+      //when player wins
+    } else if (guess === secretNumber2) {
+      displayMessage('🎉 correct number!');
+      displayNumber(secretNumber2);
+      document.querySelector('body').style.backgroundColor = '#22c376';
+      document.querySelector('.number').style.width = '15rem';
+
+      if (score2 > highscore2) {
+        highscore2 = score2;
+        document.querySelector('.highscore').textContent = highscore2;
+      }
+
+      //when guess is wrong
+    } else if (guess !== secretNumber2) {
+      if (score2 > 1) {
+        displayMessage(guess > secretNumber2 ? '📈 Too high!' : '📉Too low!');
+        score2--;
+        displayScore(score2);
+      } else {
+        displayMessage('💣 you lost the game');
+        displayScore(0);
+      }
+    }
+  });
+
+  document.querySelector('.again').addEventListener('click', function () {
+    score2 = 10;
+    secretNumber2 = Math.trunc(Math.random() * 40) + 1;
+
+    displayMessage('Start guessing…');
+    displayScore(score2);
+    displayNumber('?');
+    document.querySelector('.guess').value = '';
+
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '12rem';
+  });
+});
+
+//for hard level
+let secretNumber3 = Math.trunc(Math.random() * 100) + 1;
+let score3 = 5;
+let highscore3 = 0;
+
+document.querySelector('.hardbox').addEventListener('click', function () {
+  document.querySelector('.between').textContent = '(From 1 to 100)';
+  document.querySelector('.score').textContent = 5;
+
+  document.querySelector('.check').addEventListener('click', function () {
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess, typeof guess);
+
+    //when no input
+    if (!guess) {
+      displayMessage('⛔ No number!');
+
+      //when player wins
+    } else if (guess === secretNumber2) {
+      displayMessage('🎉 correct number!');
+      displayNumber(secretNumber3);
+      document.querySelector('body').style.backgroundColor = '#22c376';
+      document.querySelector('.number').style.width = '15rem';
+
+      if (score3 > highscore3) {
+        highscore3 = score3;
+        document.querySelector('.highscore').textContent = highscore3;
+      }
+
+      //when guess is wrong
+    } else if (guess !== secretNumber3) {
+      if (score3 > 1) {
+        displayMessage(guess > secretNumber3 ? '📈 Too high!' : '📉Too low!');
+        score3--;
+        displayScore(score3);
+      } else {
+        displayMessage('💣 you lost the game');
+        displayScore(0);
+      }
+    }
+  });
+
+  document.querySelector('.again').addEventListener('click', function () {
+    score3 = 5;
+    secretNumber2 = Math.trunc(Math.random() * 100) + 1;
+
+    displayMessage('Start guessing…');
+    displayScore(score3);
+    displayNumber('?');
+    document.querySelector('.guess').value = '';
+
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '12rem';
+  });
+});
